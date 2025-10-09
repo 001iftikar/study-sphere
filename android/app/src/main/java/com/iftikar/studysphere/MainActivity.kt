0f78e6c1432ddcf298bd334ddb72f0ffcfcf1078
Iftikar Hussain <iftikarraza8@gmail.com>
2025-10-09 14:15:02 +0530

    Initiated Project

---
+++ android/app/src/main/java/com/iftikar/studysphere/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.iftikar.studysphere
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.iftikar.studysphere.ui.theme.StudySphereTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            StudySphereTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Text(
+                        text = "Jvm deprecated",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
---
+++ android/app/src/main/java/com/iftikar/studysphere/StudySphereApplication.kt
@@ -0,0 +1,8 @@
+package com.iftikar.studysphere
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class StudySphereApplication : Application() {
+}
